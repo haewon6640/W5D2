a660f74579e97906ecd83612a947bdad64aae6e4
AA Student <appacademy@Mini-15.local>
2022-03-22 16:21:38 -0700

    Add 04 and 05

--- lib/04_select_within_select.rb
+++ lib/04_select_within_select.rb
@@ -106,9 +106,6 @@ def population_constraint
       population > (SELECT population FROM countries WHERE name = 'Canada')
     AND
       population < (SELECT population FROM countries WHERE name = 'Poland');
-        
-      
-
   SQL
 end
 
@@ -118,5 +115,19 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+    SELECT 
+      name, continent, population
+    FROM
+      countries
+    WHERE
+      continent IN (
+        SELECT 
+          continent
+        FROM
+          countries
+        GROUP BY
+          continent
+        HAVING
+          MAX(population) < 25000000);
   SQL
 end
